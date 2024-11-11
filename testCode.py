e1cb6beaa40079143062b7b8648f6cfa768effa0
AfifuddinM <94724403+AfifuddinM@users.noreply.github.com>
2024-11-12 02:10:59 +0800

    update

--- testCode.py
+++ testCode.py
@@ -4,13 +4,13 @@ link = "badCode.py"
 with open(link,'rb') as file:
     content = file.read()
 
-prompts = f'Review this code, alert any security vulnerabilities first such as sql injection and risks of attacks, provide suggestions for improvement, coding best practices, improve readability, and maintainability. Provide code examples for your suggestion. Respond in markdown format. If the file does not have any code or does not need any changes, say "No changes needed" {content}'
+prompts = f'Review this code, Rate the code "bad","mediocre","decent","good" alert any security vulnerabilities first such as sql injection and risks of attacks, provide suggestions for improvement, coding best practices, improve readability, and maintainability. Provide code examples for your suggestion." {content}'
 
 response = ollama.generate(model='codellama:7b',prompt=prompts)
 aResponse = response['response']
 
-file_path = 'responses.txt'
+file_path = 'response2.txt'
 
-with open(file_path, 'a') as file:
+with open(file_path, 'w') as file:
     file.write(f"Response: {aResponse}\n\n")
     print(f"Response saved to {file_path}")
